fa333ad65520b110a385d5ea4d53b383343b8176
Ajifirlana <firlana89@gmail.com>
2024-10-07 12:35:06 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci-inventori/';
+$config['base_url'] = 'http://aplikasi-inventori-ci3-gratis-download.test/';
 
 /*
 |--------------------------------------------------------------------------
